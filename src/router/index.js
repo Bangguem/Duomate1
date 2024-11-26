342521d30fa9a1088b452c4081c9e7b51229f737
Bangguem <dogamnevi1234@gmail.com>
2024-11-26 11:59:03 +0900

    .

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import HelloWorld from '../components/HelloWorld.vue';
 import SignupForm from '../components/SignupForm.vue';
 import LoginForm from '@/components/LoginForm.vue';
 
 const routes = [
-    {
-        path: '/',
-        name: 'Home',
-        component: HelloWorld,
-    },
     {
         path: '/signup',
         name: 'Signup',
