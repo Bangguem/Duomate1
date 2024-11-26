a826fc6d36afda2c9f4b16d8742ee665f8c2652c
hoooooong <halla202034043@gmail.com>
2024-11-26 12:03:08 +0900

    여백 제거

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import HelloWorld from '../components/HelloWorld.vue';
 import SignupForm from '../components/SignupForm.vue';
 import LoginForm from '@/components/LoginForm.vue';
 
 const routes = [
-    {
-        path: '/',
-        name: 'Home',
-        component: HelloWorld,
-    },
+    
     {
         path: '/signup',
         name: 'Signup',
