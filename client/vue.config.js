d4772b5449e2ee5db73e401e880f1b67801c1f0b
Bangguem <dogamnevi1234@gmail.com>
2025-04-21 09:17:48 +0900

    .

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,6 +1,4 @@
+// vue.config.js
 module.exports = {
-  transpileDependencies: [],
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/Duo-Mate/'  // GitHub Pages에서 사용할 리포지토리 이름으로 변경
-    : '/'
-}
+  publicPath: '/'
+};
